1cbf06231475432d4baa700c2aa919e0375ed9ee
Phuens <norbu_phuntsho@wheatoncollege.edu>
2018-06-04 16:32:09 -0400

    Refactoring multicloud page

--- lexos/static/js/scripts_multicloud.js
+++ lexos/static/js/scripts_multicloud.js
@@ -1,45 +1,33 @@
-$(document).ready(function () {
+$(function(){
   // Add tooltip to the DOM
-  var tooltip = d3.select('body').append('div')
+
+  tooltip = d3.select('body').append('div')
     .attr('class', 'd3tooltip tooltip right')
     .style('opacity', 0)
   d3.select('.d3tooltip').attr('role', 'tooltip')
 
-  // Error handler (legacy code)
-  /*	$("form").submit(function(e){
-      if ($("#multicloudtopicfile").is(":checked") && $("#mcfilesselectbttnlabel").html() == ""){
-        $('#error-message').text("No MALLET topic file uploaded.");
-        $('#error-message').show().fadeOut(3000, "easeInOutCubic");
-        return false;
-      } else if ($("#multiclouduserfiles").is(":checked") && $("input[name='segmentlist']:checked").length < 1) {
-        $('#error-message').text("No documents selected from actives.");
-        $('#error-message').show().fadeOut(3000, "easeInOutCubic");
-        return false;
-      } else{
-        $("#status-visualize").css({"visibility":"visible", "z-index": "400000"});
-        return true;
-      }
-    }); */
-
   // Show filename of uploaded file
   $('.multicloud-upload').change(function (ev) {
-    filename = ev.target.files[0].name
+    const filename = ev.target.files[0].name
     $('#bttnlabel').html(filename)
   })
 
+  // trigger function when 'toggle all' radio button is checked
   $('#allCheckBoxSelector').click(function () {
     if (this.checked) {
       $('.minifilepreview:not(:checked)').trigger('click')
-    } else {
+    }
+    else {
       $('.minifilepreview:checked').trigger('click')
     }
   })
 
-  var prev = -1 // initialize variable
+
+  let prev = -1 // initialize variable
   $('#vizcreateoptions').selectable({
     filter: 'label',  // Makes the label tags the elts that are selectable
     selecting: function (e, ui) {
-      var currnet = $(ui.selecting.tagName, e.target).index(ui.selecting)   // gets index of current taget label
+      let currnet = $(ui.selecting.tagName, e.target).index(ui.selecting)   // gets index of current target label
       if (e.shiftKey && prev > -1) {      // if you were holding the shift key and there was a box previously clicked
         // take the slice of labels from index prev to index curr and give them the 'ui-selected' class
         $(ui.selecting.tagName, e.target).slice(Math.min(prev, currnet) + 1, Math.max(prev, currnet) + 1).addClass('ui-selected')
@@ -53,46 +41,67 @@ $(document).ready(function () {
       $('.ui-selected input').trigger('click')
     }
   })
-
   // Display the document selection options on page load
   $('#multicloud-selection').show()
   $('#multicloud-upload').hide()
+
 })
 
-function renderClouds(dataset, wordCounts) {
-  // Decrease the first wordScale domain numbers to increase size contrast
-  wordScale = d3.scale.linear().domain([1, 5, 50, 500]).range([10, 20, 40, 80]).clamp(true)
-  wordColor = d3.scale.linear().domain([10, 20, 40, 80]).range(['blue', 'green', 'orange', 'red'])
 
-  numSegments = dataset.length
-  // console.log(numSegments + ' segments');
+function renderClouds (dataset, wordCounts) {
+  // Decrease the first wordScale domain numbers to increase size contrast
+  let wordScale = d3.scale.linear().domain([1, 5, 50, 500]).range([10, 20, 40, 80]).clamp(true)
+  let wordColor = d3.scale.linear().domain([10, 20, 40, 80]).range(['blue', 'green', 'orange', 'red'])
+  let numSegments = dataset.length
+  let viz
 
   $('<ul id="sortable">').appendTo('#multicloud-container')
-
-  for (i = 0; i < numSegments; i++) {
+  for (let i = 0; i < numSegments; i++) {
     $('<li class="ui-state-default" id="cloud' + i + '">').appendTo('#sortable')
-
     viz = d3.select('#cloud' + i).append('svg')
       .attr('width', 300)
       .attr('height', 380)
       .attr('id', 'svg' + i)
   }
 
-  for (i = 0; i < numSegments; i++) {
+  drawWords(wordScale, wordColor, wordCounts,dataset,wordCounts, numSegments)
+
+
+  const sort = $('#sortable')
+  sort.sortable({revert: 100})
+  sort.disableSelection()
+
+  // message appears when there is at leat one document generated.
+  if ($('#svg0')[0]) {
+    $('#tips').html('<p>Drag the clouds to rearrange them.</p>')
+  }
+
+}
+
+
+// Make pre-Ajax implementation work
+$(window).on('load', function (dataset, wordCounts) {
+  renderClouds(dataset, wordCounts)
+})
+
+
+function drawWords (wordScale, wordColor, wordCount ,dataset, wordCounts, numSegments) {
+  let statusText
+  let segment
+  let label
+  let children
+  for (let i = 0; i < numSegments; i++) {
     statusText = d3.select('#status')
     segment = dataset[i]
     label = segment['name']
     children = segment['children']
-		/* This array is now generated on the server and supplied in the
-		   ajax response. However, the client-side function is kept for reference. */
-    // wordCounts = constructWordCounts(children);
-
-    function draw(words) {
+    /* This array is now generated on the server and supplied in the
+        ajax response. However, the client-side function is kept for reference. */
+      // wordCounts = constructWordCounts(children)
+    function draw (words) {
       // Create the tooltip
-      var tooltip = d3.select('body').select('div.d3tooltip').attr('id', i)
-
-      var viz = d3.select('#svg' + i)
-
+      let tooltip = d3.select('body').select('div.d3tooltip').attr('id', i)
+      let viz = d3.select('#svg' + i)
       viz.append('g')
         .attr('transform', 'translate(150,190)')
         .attr('class', 'bigG' + i)
@@ -119,61 +128,34 @@ function renderClouds(dataset, wordCounts) {
             .style('opacity', 1)
           tooltip.html('<div class="tooltip-arrow"></div><div class="tooltip-inner">' + (d.text) + ': ' + (d.size) + '</div>')
         })
-        .on('mousemove', function (d) {
+        .on('mousemove', function () {
           return tooltip
             .style('left', (d3.event.pageX + 5) + 'px')
             .style('top', (d3.event.pageY - 20) + 'px')
-        })
-        .on('mouseout', function (d) {
-          tooltip.transition()
-            .duration(200)
-            .style('opacity', 0)
-        })
+          })
+          .on('mouseout', function () {
+            tooltip.transition()
+              .duration(200)
+              .style('opacity', 0)
+          })
 
-      viz.append('text')
-        .data(label)
-        .style('font-size', 14)
-        .style('font-weight', 900)
-        .attr('x', 65) // 100
-        .attr('y', 20) // 15
-        // .attr("x", function(){ return 150-this.getBBox().width/2; })
-        .text(function (d) {
-          return escape(label)
-          // return decodeURIComponent(escape(label));
-        })
+        viz.append('text')
+          .data(label)
+          .style('font-size', 14)
+          .style('font-weight', 900)
+          .attr('x', 65) // 100
+          .attr('y', 20) // 15
+          // .attr("x", function(){ return 150-this.getBBox().width/2; })
+          .text(function () {
+            return encodeURI(label) // encodes the string.
+          })
+      }
+
+      d3.layout.cloud().size([280, 290])
+        .words(children)
+        .rotate(function () { return ~~(Math.random() * 2) * 5 })
+        .fontSize(function (d) { return wordScale(d.size) })
+        .on('end', draw)
+        .start()
     }
-
-    d3.layout.cloud().size([280, 290])
-      .words(children)
-      .rotate(function () { return ~~(Math.random() * 2) * 5 })
-      .fontSize(function (d) { return wordScale(d.size) })
-      .on('end', draw)
-      .start()
   }
-
-  if ($('#svg0')[0]) {
-    $('#tips').html('<p>Drag the clouds to rearrange them.</p>')
-  }
-
-  /* For reference: the wordCounts array is now generated server-side. */
-	/* function constructWordCounts(list) {
-		wordCounts = {};
-
-		for (var i = 0; i < list.length; i++) {
-			word = list[i].text;
-			count = list[i].size;
-
-			wordCounts[word] = count;
-		}
-
-		return wordCounts;
-	} */
-
-  $('#sortable').sortable({ revert: 100 })
-  $('#sortable').disableSelection()
-}
-
-// Make pre-Ajax implementation work
-$(window).on('load', function (dataset, wordCounts) {
-  renderClouds(dataset, wordCounts)
-})
